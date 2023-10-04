bffb0b45090b470369fb9e6ea9ec6d379bb29740
juanpazvilla <juanpablo25paz@hotmail.com>
2023-10-04 09:47:52 -0500

    script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Initializes the carousel when the document is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
   var myCarousel = new bootstrap.Carousel(
-    document.getElementById("peliculasCarousel"),
+    document.getElementById("moviesCarousel"),
     {
       interval: 3000,
     }
